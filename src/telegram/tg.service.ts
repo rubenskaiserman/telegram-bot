9074e76a431a18731ffc0a7267ec696547707312
kaiserman <rubskaiserman@gmail.com>
2023-07-30 18:56:56 -0300

    testing axios

--- src/telegram/tg.service.ts
+++ src/telegram/tg.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+import axios from 'axios';
 
 // For now I don't want to use a blob management tool. So I'll just leave the about text here.
 const about = "Olá! Sou o Rubens Kaiserman, um entusiasta da tecnologia e um desenvolvedor FullStack com ampla experiência na stack (TypeScript) NestJS, NextJS + React, AWS Dynamo + MySQL. Minha paixão recente tem sido a construção de sistemas SaaS, onde utilizo essa stack para desenvolver softwares web no modelo de microsserviços modularizados, garantindo uma comunicação eficiente entre os componentes.\n\nAlém disso, sou especializado em Python e o utilizo para implementar Robotic Process Automation (RPA), reduzindo a necessidade de retrabalho ou processos excessivos e aumentando significativamente a eficiência dos projetos.\n\nIniciei minha trajetória acadêmica com formação técnica em Informática pelo IFRJ, onde tive a honra de conquistar duas menções honrosas na Olimpíada Brasileira de Matemática, destacando minha habilidade analítica desde cedo.\n\nComo desenvolvedor FullStack, possuo expertise no desenvolvimento com React, criando interfaces ricas e interativas para aprimorar a experiência dos usuários. A integração harmoniosa entre diferentes tecnologias, como MySQL e NestJS, permite que eu construa sistemas robustos e escaláveis.\n\nEstou verdadeiramente empolgado para contribuir com projetos inovadores e agregar valor ao seu time. Se busca um desenvolvedor comprometido, versátil e atualizado, estou aqui para colaborar. Vamos conversar mais sobre como posso fazer parte do seu time de sucesso! Agradeço pela oportunidade!"
@@ -14,19 +15,28 @@ export class TelegramService {
 
   private async sendMessage(chatId:string, message:string) {
     try {
-      return await fetch(
-        'https://api.telegram.org/bot' + process.env.TG_TOKEN + '/sendMessage',
-        {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            chat_id: chatId,
-            text: message,
-          }),
-        },
-      );
+      // return await fetch(
+      //   'https://api.telegram.org/bot' + process.env.TG_TOKEN + '/sendMessage',
+      //   {
+      //     method: 'post',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     body: JSON.stringify({
+      //       chat_id: chatId,
+      //       text: message,
+      //     }),
+      //   },
+      // );
+      return axios.post('https://api.telegram.org/bot' + process.env.TG_TOKEN + '/sendMessage', {
+        chat_id: chatId,
+        text: message,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
     } catch {
       console.log('Error sending message to Telegram');
       await this.sendMessage(chatId, message); // Shouldn't do it but doing temporarily
@@ -46,7 +56,7 @@ export class TelegramService {
     // answers user with GPT-3 response
   }
 
-  about(chat_id) {
+  about(chat_id:string) {
     this.sendMessage(chat_id, about);
   }
 
